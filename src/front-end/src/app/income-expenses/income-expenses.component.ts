da9daba0e70e178612943ea54f6664a2aee10228
Daniys <dani_y3@hotmail.com>
2021-05-21 23:23:29 -0300

    implementado card

--- src/front-end/src/app/income-expenses/income-expenses.component.ts
+++ src/front-end/src/app/income-expenses/income-expenses.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Extrato } from '../model/extrato';
 @Component({
   selector: 'app-income-expenses',
   templateUrl: './income-expenses.component.html',
   styleUrls: ['./income-expenses.component.css']
 })
 export class IncomeExpensesComponent implements OnInit {
-
+  extrato:Extrato[]=[]
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  adicionarDado(extratoDado){
-    console.log(extratoDado);
-  }
+  adicionarDado(extratoForm){
+    const dado: Extrato = {
+      descricao: extratoForm.value.dado,
+      valor:extratoForm.value.dadoValue,
+      tipo: false
+      };
+      this.extrato.push(dado);
+      extratoForm.resetForm();
+      
+    }
+  
 
 }
